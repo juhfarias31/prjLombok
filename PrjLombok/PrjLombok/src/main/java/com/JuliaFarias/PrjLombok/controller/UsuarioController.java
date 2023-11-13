7fe1cdbdfe7ac093c6327299d0939fe6eaff0619
Julia Farias de Queiroz <133894616+juhfarias31@users.noreply.github.com>
2023-11-13 15:52:43 -0300

    Add files via upload

---
+++ PrjLombok/PrjLombok/src/main/java/com/JuliaFarias/PrjLombok/controller/UsuarioController.java
@@ -0,0 +1,70 @@
+package com.JuliaFarias.PrjLombok.controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.JuliaFarias.PrjLombok.entities.Usuario;
+
+import jakarta.validation.Valid;
+
+public class UsuarioController {
+
+	private final Service usuarioService;
+
+	@Autowired
+	public UsuarioController(Service usuarioService) {
+		this.usuarioService = usuarioService;
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> findUsuariobyId(@PathVariable Long id) {
+		Usuario usuario = usuarioService.findUsuarioById(id);
+		if (usuario != null) {
+			return ResponseEntity.ok(usuario);
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	@GetMapping
+	public ResponseEntity<List<Usuario>> findAllUsuarioscontrol() {
+		List<Usuario> usuarios = usuarioService.findAllUsuario();
+		return ResponseEntity.ok(usuarios);
+	}
+
+	@PostMapping
+	public ResponseEntity<Usuario> insertUsuariosControl(@RequestBody @Valid Usuario usuario) {
+		Usuario novoUsuario = usuarioService.insertUsuario(usuario);
+		return ResponseEntity.status(HttpStatus.CREATED).body(novoUsuario);
+	}
+
+	@PutMapping("/id")
+	public ResponseEntity<Usuario> updateUsuarioControl(@PathVariable Long id, @RequestBody @Valid Usuario usuario) {
+		Usuario mudausuario = usuarioService.updateUsuario(id, usuario);
+		if (mudausuario != null) {
+			return ResponseEntity.ok(usuario);
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	@DeleteMapping("/id")
+	public ResponseEntity<String> deleteUsuarioControl(@PathVariable Long id) {
+		boolean remover = usuarioService.deleteUsuario(id);
+		if (remover) {
+			return ResponseEntity.ok().body("usuario Excluido com sucesso");
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+}
---
+++ PrjLombok/PrjLombok/src/test/java/com/JuliaFarias/PrjLombok/PrjLombokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JuliaFarias.PrjLombok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjLombokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
